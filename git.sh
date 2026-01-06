bae4bb9fd1369f1279313f801c94464323922dee
rmordona@gmail.com <raymondordona@Raymonds-iMac.local>
2026-01-06 07:25:39 -0800

    (POC) UI Platform - Rendering and Routing Corrections, ReactFlow initial design (Enhancing the graph with control panel navigations)

--- git.sh
+++ git.sh
@@ -1,3 +1,4 @@
+git config --global http.postBuffer 524288000
 git add --all
 git commit -m "(POC) UI Platform - Rendering and Routing Corrections, ReactFlow initial design (Enhancing the graph with control panel navigations)"
 git push -u origin main
