be98d0026667425acdbe9fd2b8ddd138d00a6edd
rmordona@gmail.com <raymondordona@Raymonds-iMac.local>
2026-01-26 00:43:48 -0800

    (POC) Adding dynamic pipelining with living documents (Correcting Pipeline and Agent Parsers)

--- git.sh
+++ git.sh
@@ -1,4 +1,4 @@
 git config --global http.postBuffer 524288000
 git add --all
-git commit -m "(POC) Adding dynamic pipelining with living documents (Added Data and Tool Envelope, AgentRunner,Planner,Validator)"
+git commit -m "(POC) Adding dynamic pipelining with living documents (Correcting Pipeline and Agent Parsers)"
 git push -u origin main
